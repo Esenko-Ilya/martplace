a4fda12dcccf1f824d28ed6b38c590adfe01203d
Ilya Esenko <esenkoilya@gmail.com>
2020-04-12 20:09:20 +0300

    update last version + add search and slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,19 @@
 $(function(){
 
+  $('.slider-weekly__inner').slick({
+       
+  });
 
+  $('.search__select').select2({
+         width: '195px',
+      });
+
+  $('.rate-star').rateYo({
+        starWidth: '15px',
+        readOnly: true,
+        rating: 4.5,
+        spacing: '3px'
+      });
 
   
 });
